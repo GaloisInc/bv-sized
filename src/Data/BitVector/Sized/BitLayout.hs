d7e7cf345e4b5b10dcf2bc346aca2a6935cdb14d
Ben Selfridge <benselfridge@galois.com>
2018-03-10 22:05:18 -0800

    another show instance fix

--- src/Data/BitVector/Sized/BitLayout.hs
+++ src/Data/BitVector/Sized/BitLayout.hs
@@ -65,7 +65,7 @@ chunk start = Chunk knownNat start
 instance Show (Chunk w) where
   show (Chunk wRepr start)
     | width > 0 =
-      "[" ++ show start ++ "..." ++ show (start + width - 1) ++ "]"
+      "[" ++ show (start + width - 1) ++ "..." ++ show start ++ "]"
     | otherwise = "[" ++ show start ++ "]"
     where width = fromIntegral (natValue wRepr)
 
